0eea1bd4aa2c061948c1eb9ba348fecf2589bd05
Casey Manning <kcgrind@gmail.com>
2022-04-12 21:25:48 -0700

    in-moment tracking

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
+var db;
+
 function showArchives() {
     document.getElementById("box1").classList.add("hidden");
     document.getElementById("box2").classList.remove("hidden");
     inArchive = true;
 }
+
+function initFirestore() {
+
+    firebase.initializeApp({
+        apiKey: "AIzaSyCmt8vmu_jtPJM5meZ74dCPXcRMLBx5pw4",
+        authDomain: "tracker-386d8.firebaseapp.com",
+        projectId: "tracker-386d8"
+      });
+      
+    db = firebase.firestore();
+}
+
+initFirestore();
+
+function logVisit() {
+    db.collection("moments").add({
+        action: "menuload",
+        time: firebase.firestore.FieldValue.serverTimestamp()
+    })
+    .then((docRef) => {
+        console.log("Document written with ID: ", docRef.id);
+    })
+    .catch((error) => {
+        console.error("Error adding document: ", error);
+    });
+}
+
+logVisit()
---
+++ momentscr.js
@@ -0,0 +1,20 @@
+var db;
+
+firebase.initializeApp({
+    apiKey: "AIzaSyCmt8vmu_jtPJM5meZ74dCPXcRMLBx5pw4",
+    authDomain: "tracker-386d8.firebaseapp.com",
+    projectId: "tracker-386d8"
+    });
+    
+db = firebase.firestore();
+
+db.collection("moments").add({
+    moment: document.title,
+    time: firebase.firestore.FieldValue.serverTimestamp()
+})
+.then((docRef) => {
+    console.log("Document written with ID: ", docRef.id);
+})
+.catch((error) => {
+    console.error("Error adding document: ", error);
+});
